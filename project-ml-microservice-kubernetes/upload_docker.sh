6eba0c42996f80f9d19d703324392bf3b1d724c1
Joseph Chung <josephchung@Jo-MacBook-Pro.local>
2022-11-05 08:26:21 +0900

    initial commit

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=itjosephchung/ml-api
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u itjosephchung
+docker tag ml-api $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
